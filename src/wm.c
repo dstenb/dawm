ef36134adf74820aa655e2691f2285a10867528c
dstenb <dstenb@gmail.com>
2012-06-05 22:30:52 +0200

    added wm_checkotherwm()

--- src/wm.c
+++ src/wm.c
@@ -1,6 +1,31 @@
 #include "wm.h"
 
 static unsigned long wm_getcolor(struct wm *, const char *);
+static void wm_checkotherwm(struct wm *);
+static int wm_xerror_checkotherwm(Display *, XErrorEvent *);
+static int wm_xerror(Display *, XErrorEvent *);
+
+void wm_checkotherwm(struct wm *wm)
+{
+	XSetErrorHandler(wm_xerror_checkotherwm);
+	/* this causes an error if some other window manager is running */
+	XSelectInput(wm->dpy, DefaultRootWindow(wm->dpy),
+			SubstructureRedirectMask);
+	XSync(wm->dpy, False);
+	XSetErrorHandler(wm_xerror);
+	XSync(wm->dpy, False);
+}
+
+int wm_xerror(Display *dpy, XErrorEvent *ee)
+{
+
+}
+
+int wm_xerror_checkotherwm(Display *dpy, XErrorEvent *ee)
+{
+	die("another window manager is running\n");
+	return 0;
+}
 
 unsigned long wm_getcolor(struct wm *wm, const char *str) {
 	Colormap cmap = DefaultColormap(wm->dpy, wm->screen);
@@ -21,17 +46,18 @@ struct wm *wm_init(void)
 	if (!(wm->dpy = XOpenDisplay(NULL)))
 		die("couldn't open display '%s'\n", getenv("DISPLAY"));
 
-	/* TODO check for other window managers */
+	/* check if another wm is running */
+	wm_checkotherwm(wm);
 
 	wm->screen = DefaultScreen(wm->dpy);
 	wm->root = RootWindow(wm->dpy, wm->screen);
 	wm->width = DisplayWidth(wm->dpy, wm->screen);
 	wm->height = DisplayHeight(wm->dpy, wm->screen);
+	wm->restart = 0;
 
 	printf("wm->width: %i\n", wm->width);
 	printf("wm->height: %i\n", wm->height);
 
-	wm->restart = 0;
 
 	return wm;
 }
