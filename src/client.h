0402f9f411e687a6f39820f7bad395ed7ba79a0c
dstenb <dstenb@gmail.com>
2012-06-06 16:41:07 +0200

    added some data to the client struct

--- src/client.h
+++ src/client.h
@@ -4,9 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "common.h"
+
 struct client {
+	struct rect cur_r; /* current position and size */
+	struct rect old_r; /* old position and size */
 
+	int floating; /* non-zero if floating */
+	int fullscreen; /* non-zero if fullscreen */
 
+	struct client *next;
 };
 
 #endif
