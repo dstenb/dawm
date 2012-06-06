4764d6caa2b52bf67f56155b3068dbb8ba20d655
dstenb <dstenb@gmail.com>
2012-06-06 14:06:54 +0200

    added handler functions

--- src/wm.c
+++ src/wm.c
@@ -8,11 +8,40 @@ static unsigned long wm_getcolor(struct wm *, const char *);
 static void wm_checkotherwm(struct wm *);
 static int wm_xerror_checkotherwm(Display *, XErrorEvent *);
 static int wm_xerror(Display *, XErrorEvent *);
-static void wm_handler_keypress(struct wm *, XEvent *);
+
+/* X handlers */
 static void wm_handler_buttonpress(struct wm *, XEvent *);
+static void wm_handler_clientmessage(struct wm *, XEvent *);
+static void wm_handler_configurerequest(struct wm *, XEvent *);
+static void wm_handler_configurenotify(struct wm *, XEvent *);
+static void wm_handler_destroynotify(struct wm *, XEvent *);
+static void wm_handler_enternotify(struct wm *, XEvent *);
+static void wm_handler_expose(struct wm *, XEvent *);
+static void wm_handler_focusin(struct wm *, XEvent *);
+static void wm_handler_keypress(struct wm *, XEvent *);
+static void wm_handler_mappingnotify(struct wm *, XEvent *);
+static void wm_handler_maprequest(struct wm *, XEvent *);
 static void wm_handler_motionnotify(struct wm *, XEvent *);
+static void wm_handler_propertynotify(struct wm *, XEvent *);
+static void wm_handler_unmapnotify(struct wm *, XEvent *);
 
 static int (*xerrxlib) (Display *, XErrorEvent *);
+static void (*handler[LASTEvent]) (struct wm *, XEvent *) = {
+	[ButtonPress] = wm_handler_buttonpress,
+	[ClientMessage] = wm_handler_clientmessage,
+	[ConfigureRequest] = wm_handler_configurerequest,
+	[ConfigureNotify] = wm_handler_configurenotify,
+	[DestroyNotify] = wm_handler_destroynotify,
+	[EnterNotify] = wm_handler_enternotify,
+	[Expose] = wm_handler_expose,
+	[FocusIn] = wm_handler_focusin,
+	[KeyPress] = wm_handler_keypress,
+	[MappingNotify] = wm_handler_mappingnotify,
+	[MapRequest] = wm_handler_maprequest,
+	[MotionNotify] = wm_handler_motionnotify,
+	[PropertyNotify] = wm_handler_propertynotify,
+	[UnmapNotify] = wm_handler_unmapnotify
+};
 
 void wm_checkotherwm(struct wm *wm)
 {
@@ -24,21 +53,8 @@ void wm_checkotherwm(struct wm *wm)
 	XSync(wm->dpy, False);
 }
 
-int wm_xerror(Display *dpy, XErrorEvent *ee)
+unsigned long wm_getcolor(struct wm *wm, const char *str)
 {
-	error("fatal error: request code=%d, error code=%d\n",
-			ee->request_code, ee->error_code);
-	return xerrxlib(dpy, ee); /* may call exit */
-
-}
-
-int wm_xerror_checkotherwm(Display *dpy, XErrorEvent *ee)
-{
-	die("another window manager is running\n");
-	return 0;
-}
-
-unsigned long wm_getcolor(struct wm *wm, const char *str) {
 	Colormap cmap = DefaultColormap(wm->dpy, wm->screen);
 	XColor color;
 
@@ -90,24 +106,9 @@ int wm_eventloop(struct wm *wm)
 
 	for (;;) {
 		XNextEvent(wm->dpy, &ev);
-		error("ABC\n");
-
-		switch(ev.type) {
-			case KeyPress:
-				wm_handler_keypress(wm, &ev);
-				break;
-			case ButtonPress:
-				wm_handler_buttonpress(wm, &ev);
-				break;
-			case MotionNotify:
-				wm_handler_motionnotify(wm, &ev);
-				break;
-			default:
-				break;
-		}
+		if (handler[ev.type])
+			handler[ev.type](wm, &ev);
 	}
-
-	(void)wm;
 	return 0;
 }
 
@@ -117,19 +118,116 @@ int wm_destroy(struct wm *wm)
 	return 0;
 }
 
-void wm_handler_keypress(struct wm *wm, XEvent *ev)
-{
-	error("keypress handler");
-}
-
 void wm_handler_buttonpress(struct wm *wm, XEvent *ev)
 {
-	error("buttonpress handler");
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_clientmessage(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_configurerequest(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_configurenotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_destroynotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_enternotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_expose(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_focusin(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_keypress(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+
+}
+
+void wm_handler_mappingnotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+void wm_handler_maprequest(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
 }
 
 void wm_handler_motionnotify(struct wm *wm, XEvent *ev)
 {
-	error("motionnotify handler");
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
 }
 
+void wm_handler_propertynotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
 
+void wm_handler_unmapnotify(struct wm *wm, XEvent *ev)
+{
+	(void)wm;
+	(void)ev;
+	error("%s\n", __func__);
+}
+
+int wm_xerror(Display *dpy, XErrorEvent *ee)
+{
+	error("fatal error: request code=%d, error code=%d\n",
+			ee->request_code, ee->error_code);
+	return xerrxlib(dpy, ee); /* may call exit */
+}
+
+int wm_xerror_checkotherwm(Display *dpy, XErrorEvent *ee)
+{
+	(void)dpy;
+	(void)ee;
+	die("another window manager is running\n");
+	return 0;
+}
