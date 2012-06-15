c0b409571c308a846bbbe95e580e80c191392f26
dstenb <dstenb@gmail.com>
2012-06-15 21:53:15 +0200

    Missed to add colors.[ch]

---
+++ src/colors.c
@@ -0,0 +1,53 @@
+#include "colors.h"
+
+static unsigned long get_color(Display *, int, const char *);
+
+static unsigned long colors[LASTColor];
+
+unsigned long get_color(Display *dpy, int screen, const char *str)
+{
+	Colormap cmap = DefaultColormap(dpy, screen);
+	XColor c;
+
+	if(!XAllocNamedColor(dpy, cmap, str, &c, &c))
+		die("error, cannot allocate color '%s'\n", str);
+	return c.pixel;
+}
+
+/* get cursor with the given id */
+unsigned long color(ColorID id)
+{
+	assert(id < LASTColor);
+	return colors[id];
+}
+
+/* initializes the cursors */
+void colors_init(char *colorstr[LASTColor], Display *dpy, int screen)
+{
+	int i;
+
+	for (i = 0; i < LASTColor; i++)
+		colors[i] = get_color(dpy, screen, colorstr[i]);
+}
+
+int str_to_color_id(const char *str)
+{
+	int i;
+
+	static const char *colorstr[] = {
+		"bar_border",
+		"bar_norm_fg",
+		"bar_norm_bg",
+		"bar_sel_fg",
+		"bar_sel_bg",
+		"win_norm_border",
+		"win_sel_border"
+	};
+
+	for (i = 0; i < ARRSIZE(colorstr); i++) {
+		if (colorstr[i] && STREQ(colorstr[i], str))
+			return i;
+	}
+
+	return InvalidColor;
+}
---
+++ src/colors.h
@@ -0,0 +1,32 @@
+#ifndef _COLORS_H_
+#define _COLORS_H_
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/cursorfont.h>
+#include <X11/Xlib.h>
+
+#include "utils.h"
+
+typedef enum {
+	BarBorder,
+	BarNormFG,
+	BarNormBG,
+	BarSelFG,
+	BarSelBG,
+	WinNormBorder,
+	WinSelBorder,
+	LASTColor,
+	InvalidColor = -1
+} ColorID;
+
+/* get cursor with the given id */
+unsigned long color(ColorID);
+
+/* initializes the cursors */
+void colors_init(char *[LASTColor], Display *, int);
+
+int str_to_color_id(const char *);
+
+#endif
