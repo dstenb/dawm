55c891678b34225ce4bd1225354c7b49a5cfa922
dstenb <dstenb@gmail.com>
2012-08-13 20:13:19 +0200

    Added generate-version.sh

---
+++ src/generate-version.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+echo "#ifndef _VERSION_H_" >  .version.h
+echo "#define _VERSION_H_" >> .version.h
+echo -n "#define VERSION \"" >> .version.h
+git show -s --pretty=format:"commit %h (%ai)\"%n" >> .version.h
+echo "#endif" >> .version.h
+
+if [ ! -e version.h ] || [ "`diff -q version.h .version.h`" ]; then
+	echo "hej"
+	cp .version.h version.h
+fi
+
+rm .version.h
